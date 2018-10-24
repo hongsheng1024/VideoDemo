901e679c823b591aeb8df5e868268ede4c1307e9
yanghuan <278432876@qq.com>
2018-10-24 14:22:36 +0800

    初次提交

---
+++ VideoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VideoDemo
+//
+//  Created by yanghuan on 2018/10/24.
+//  Copyright © 2018 whs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VideoDemo/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  VideoDemo
+//
+//  Created by yanghuan on 2018/10/24.
+//  Copyright © 2018 whs. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"视频测试";
+    
+    
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
